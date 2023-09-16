bbb7f8f4d9a1a5aff911d280219b6ad553d176fe
Paul Fadayo <paulfadayo@yahoo.com>
2023-09-16 23:42:21 +0100

    updating diffing algorithm

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
